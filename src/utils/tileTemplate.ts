4f83eba9a627ebc283d6a91ef463f37c0bd07928
Momin2004 <momin23204@gmail.com>
2026-02-21 20:37:26 +0100

    Add template for tiles

---
+++ src/utils/tileTemplate.ts
@@ -0,0 +1,105 @@
+import { Tile, TileType } from "@/types/board";
+
+export const tileTemplate: Tile[] =
+  [
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.Red
+    },
+    {
+      flipped: false,
+      type: TileType.Blue
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.Black
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    },
+    {
+      flipped: false,
+      type: TileType.White
+    }
+  ]
