b4af1afcac73d53ab3decda96ff3acdd04dae5c8
Momin2004 <momin23204@gmail.com>
2026-03-08 19:59:50 +0100

    Move main back to src

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import App from "./src/App";
+import App from "./App";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { theme } from "./src/utils/theme";
+import { theme } from "./utils/theme";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
 
